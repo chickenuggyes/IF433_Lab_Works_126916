030315dbc176ef9d713799587fdae39934c36152
carolsndra <olinechen9@gmail.com>
2026-02-04 16:33:01 +0700

    week01: initial setup and procedural code

---
+++ src/OOP_126916_CarolineSandraChen/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package OOP_126916_CarolineSandraChen.week01
+
+fun main() {
+    var name: String = "Caroline Sandra"
+    var score: Int = 80
+
+    println("Nama: " + name + ", Nilai: " + score)
+}
