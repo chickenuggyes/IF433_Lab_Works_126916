fa0b1ddf88f0c1257d186c155686269762fcf846
carolsndra <olinechen9@gmail.com>
2026-02-11 15:51:50 +0700

    week02: add secondary constructor

--- src/OOP_126916_CarolineSandraChen/week02/Student.kt
+++ src/OOP_126916_CarolineSandraChen/week02/Student.kt
@@ -14,4 +14,8 @@ class Student (
             println("LOG: Objek student $name berhasil dialokasikan di Memory")
         }
     }
+
+    constructor(name: String, nim : String) : this(name, nim, "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
 }
