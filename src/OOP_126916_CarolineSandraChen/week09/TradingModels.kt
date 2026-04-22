57ff1236298e2cbd68353dab9a4671543d71a78c
carolsndra <olinechen9@gmail.com>
2026-04-22 10:45:35 +0700

    week09: (task) create TradeLog data class

---
+++ src/OOP_126916_CarolineSandraChen/week09/TradingModels.kt
@@ -0,0 +1,9 @@
+package OOP_126916_CarolineSandraChen.week09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String,
+)
