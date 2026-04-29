7cf4e36ccb365699709a871850e35f32964900d6
carolsndra <olinechen9@gmail.com>
2026-04-29 11:43:18 +0700

    week10: (task) populate coin repository

--- src/OOP_126916_CarolineSandraChen/week10/CryptoDashboard.kt
+++ src/OOP_126916_CarolineSandraChen/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package OOP_126916_CarolineSandraChen.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.75))
+    coinRepo.add(Coin("ETH", 4.25))
+    coinRepo.add(Coin("USDT", 1500.0))
 }
