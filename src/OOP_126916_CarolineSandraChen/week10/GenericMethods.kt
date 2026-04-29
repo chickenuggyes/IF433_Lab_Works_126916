aae6cbb71d4457a141d621d0bcf42e927f20620f
carolsndra <olinechen9@gmail.com>
2026-04-29 11:11:47 +0700

    week10: implement where clause for comparable constraints

--- src/OOP_126916_CarolineSandraChen/week10/GenericMethods.kt
+++ src/OOP_126916_CarolineSandraChen/week10/GenericMethods.kt
@@ -7,3 +7,7 @@ fun <T> printData(data: T) {
 fun <T> processData(input: T): T {
     return input
 }
+
+fun <T> getMaximum(value1: T, value2: T): T where T : Comparable<T> {
+    return if (value1 >= value2) value1 else value2
+}
