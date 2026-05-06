152bbd96e3737ad4054fc0ce72ada006c488bddf
carolsndra <olinechen9@gmail.com>
2026-05-06 09:36:15 +0700

    week11: (task) create SmartDevice data class

---
+++ src/OOP_126916_CarolineSandraChen/week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package OOP_126916_CarolineSandraChen.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
